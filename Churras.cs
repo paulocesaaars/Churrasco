5ce0099665ab3b97da9b14ece9d84c0d91cdcc0b
Paulo César de Souza <paulocesaaars@gmail.com>
2022-04-01 07:41:38 -0300

    Update Churras.cs

--- Churras.cs
+++ Churras.cs
@@ -22,29 +22,29 @@
 		{
 			var convidados = new List<Convidado>(22);
 
-			convidados.Add(new Convidado("Matheus",             "confirmado", true));
-			convidados.Add(new Convidado("Solange",             "confirmada", true));
-			convidados.Add(new Convidado("Cristofer",           "confirmado", true));
-			convidados.Add(new Convidado("Yuki",                "confirmada", true));
-			convidados.Add(new Convidado("Paulo",               "confirmado", true));
-			convidados.Add(new Convidado("Paulo - Esposa",      "confirmada", true));
-			convidados.Add(new Convidado("Paulo - Filha",       "confirmada", true));
-			convidados.Add(new Convidado("Paulo - Sogro",       "confirmado", true));
-			convidados.Add(new Convidado("Paulo - Sogra",       "confirmada", true));
-			convidados.Add(new Convidado("Paulo - cunhado",     "confirmada", true));
-			convidados.Add(new Convidado("João Lucas",          "confirmado", true));
-			convidados.Add(new Convidado("João Lucas - Esposa", "confirmada", true));
-			convidados.Add(new Convidado("João Lucas - Filho",  "confirmado", true));
-			convidados.Add(new Convidado("Sergio",              "confirmado", true));
-			convidados.Add(new Convidado("Sergio - Esposa",     "confirmada", true));
-			convidados.Add(new Convidado("Sergio - Filho",      "confirmado", true));
-			convidados.Add(new Convidado("Bruno",               "confirmado", true));
-			convidados.Add(new Convidado("Bruno - Esposa",      "confirmada", true));
-			convidados.Add(new Convidado("Bruno - Filha)",      "confirmada", true));
-			convidados.Add(new Convidado("Carlos",              "confirmado", false));
-			convidados.Add(new Convidado("Carlos - Esposa",     "confirmada", false));
-			convidados.Add(new Convidado("Carlos - Filha",      "confirmada", false));
-			convidados.Add(new Convidado("Carlos - Filha",      "confirmada", false));
+			convidados.Add(new Convidado("Matheus",             "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Solange",             "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Cristofer",           "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Yuki",                "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo",               "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo - Esposa",      "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo - Filha",       "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo - Sogro",       "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo - Sogra",       "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Paulo - cunhado",     "JÁ PAGOU", true));
+			convidados.Add(new Convidado("João Lucas",          "JÁ PAGOU", true));
+			convidados.Add(new Convidado("João Lucas - Esposa", "JÁ PAGOU", true));
+			convidados.Add(new Convidado("João Lucas - Filho",  "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Sergio",              "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Sergio - Esposa",     "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Sergio - Filho",      "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Bruno",               "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Bruno - Esposa",      "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Bruno - Filha)",      "JÁ PAGOU", true));
+			convidados.Add(new Convidado("Carlos",              "NÃOPAGOU", false));
+			convidados.Add(new Convidado("Carlos - Esposa",     "NÃOPAGOU", false));
+			convidados.Add(new Convidado("Carlos - Filha",      "NÃOPAGOU", false));
+			convidados.Add(new Convidado("Carlos - Filha",      "NÃOPAGOU", false));
 
 			if (DateTime.Now == new DateTime(2022,04,02))
 			{
